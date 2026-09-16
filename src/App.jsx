5f05eaea934722dabcddf377978101fd1e986949
fathimathasmaa-sudo <fathimathasmaa@gmail.com>
2026-09-16 12:49:58 +0500

    Wire delivery and payment settings

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import CustomerOrder from "./pages/CustomerOrder";
 import KitchenView from "./pages/KitchenView";
 import AdminOperations from "./pages/AdminOperations";
 import AdminMenu from "./pages/AdminMenu";
+import AdminDeliveryPayments from "./pages/AdminDeliveryPayments";
 
 export default function App() {
   return <Router><Routes>
@@ -24,6 +25,7 @@ export default function App() {
     <Route path="/admin/dashboard" element={<AdminDashboard />} />
     <Route path="/admin/operations" element={<AdminOperations />} />
     <Route path="/admin/menu" element={<AdminMenu />} />
+    <Route path="/admin/delivery-payments" element={<AdminDeliveryPayments />} />
     <Route path="/staff-dashboard" element={<StaffOrders />} />
     <Route path="/kitchen" element={<KitchenView />} />
     <Route path="/menu" element={<CustomerMenu />} />
