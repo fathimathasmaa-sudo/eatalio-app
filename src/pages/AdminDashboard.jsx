9ba232203c760f53c450d33e2a90f50a49f26609
fathimathasmaa-sudo <fathimathasmaa@gmail.com>
2026-09-16 09:24:15 +0500

    Fix AdminDashboard import typo

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,4 +1,4 @@
-giimport React, { useEffect, useMemo, useState, useMemo as useReactMemo } from "react";
+import React, { useEffect, useMemo, useState, useMemo as useReactMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import { db, app, auth } from "../firebaseConfig";
